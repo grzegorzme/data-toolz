d4b4c64176561fa70fa4ba9f37fe7b6bc9d9b753
Grzegorz Melniczak <gmelniczak@olx.pl>
2020-11-27 13:27:20 +0100

    add DataIO class

--- datatoolz/io.py
+++ datatoolz/io.py
@@ -1,28 +1,32 @@
 """Module providing a FileSystem wrapper class"""
 
+import os
+import io
 import inspect
+import time
 from uuid import uuid4
+from concurrent.futures import ThreadPoolExecutor
+from gzip import compress, decompress
+
 import botocore.session
 import s3fs
+import pandas as pd
 from fsspec import AbstractFileSystem
 from fsspec.implementations.local import LocalFileSystem
-
-LOCAL = "local"
-S3 = "s3"
 
 
 class FileSystem(AbstractFileSystem):
     """Wrapper for easier initialization of various file-system classes"""
 
-    def __init__(self, name=LOCAL, assumed_role=None, endpoint_url=None):
+    def __init__(self, name="local", assumed_role=None, endpoint_url=None):
         super().__init__()
         self.name = name
         self.assumed_role = assumed_role
         self.endpoint_url = endpoint_url
 
-        if self.name == LOCAL:
+        if self.name == "local":
             self.filesystem = LocalFileSystem()
-        elif self.name == S3:
+        elif self.name == "s3":
             session = botocore.session.get_session()
             if self.assumed_role:
                 sts = session.create_client("sts")
@@ -72,3 +76,206 @@ class FileSystem(AbstractFileSystem):
 
     def sign(self, path, expiration=100, **kwargs):
         return self.filesystem.sign(path=path, expiration=expiration, **kwargs)
+
+
+class DataIO:
+    """Class for data writing/reading"""
+
+    def __init__(
+        self,
+        filesystem=None,
+        partition_transformer=None,
+    ):
+        """
+        DataIO initializer
+        :param filesystem: optional, FileSystem object, default FileSystem("local")
+        :param partition_transformer: optional,
+          function(prefix, partitions, values, suffix): custom function
+          for transforming output partition naming
+        """
+        self.filesystem = FileSystem() if filesystem is None else filesystem
+        self.partition_transformer = (
+            self.get_path if partition_transformer is None else partition_transformer
+        )
+
+    @staticmethod
+    def get_path(prefix="", partitions=None, values=None, suffix=""):
+        """
+        Helper for formatting paths
+        :param prefix: path prefix
+        :param partitions: optional, list-like - partitioning fields
+        :param values: optional, list-like - partition field-values
+        :param suffix: optional, path-suffix (filename)
+        :return: path-like
+        e.g. ("prefix", ["a", "b"], [1, 2], "suffix") -> "prefix/a=1/b=2/suffix"
+        """
+        partitions = partitions or list()
+        values = values or list()
+        if len(partitions) != len(values) or any(v is None for v in values):
+            raise ValueError(
+                "`partitions` and `values` lengths must match "
+                "and `values` must not contain NoneType"
+            )
+
+        return os.path.join(
+            prefix,
+            *(f"{field}={value}" for field, value in zip(partitions, values)),
+            f"{time.time_ns()}-{uuid4()}" if suffix is None else suffix,
+        )
+
+    def get_partitions(
+        self, dataframe, partition_by=None, prefix="", suffix="", drop=False
+    ):
+        """
+        Partitions dataframe based on selected fields
+        :param dataframe: pandas.DataFrame
+        :param partition_by: optional, list of columns to partition the output
+        :param prefix: optional, prefix for partition name
+        :param suffix: optional, suffix for partition name
+        :param drop: bool, should the partition fields be dropped from the output
+        :return: generator of (partition_name: str, partition_data: pandas.DataFrame)
+        """
+        if partition_by is None:
+            yield prefix, dataframe
+        else:
+            for group, partition in dataframe.groupby(partition_by):
+                if isinstance(group, str):
+                    group = (group,)
+                partition_path = self.partition_transformer(
+                    prefix=prefix, partitions=partition_by, values=group, suffix=suffix
+                )
+                if drop:
+                    partition = partition.drop(partition_by, axis=1)
+                yield partition_path, partition
+
+    def read(self, path, filetype="parquet", gzip=False, header=None):
+        """
+        Function for reading (partitioned) data sets from a given path
+        :param path: path-like
+        :param filetype: input format type: parquet|tsv|jsonlines
+        :param gzip: is the input compressed, only used for tsv|jsonlines
+        :param header: bool - True if input files has headers
+        :return: pandas.DataFrame
+        """
+
+        def _deserialize(data, filetype, gzip, header):
+            if gzip:
+                data = decompress(data)
+
+            if filetype == "tsv":
+                data = pd.read_csv(
+                    io.StringIO(data.decode(encoding="utf-8")),
+                    sep="\t",
+                    header=0 if header is True else None,
+                    dtype=str,
+                    keep_default_na=False,
+                )
+            elif filetype == "jsonlines":
+                data = pd.read_json(
+                    io.StringIO(data.decode(encoding="utf-8")),
+                    orient="records",
+                    lines=True,
+                    dtype=False,
+                )
+            else:
+                raise ValueError(f"Unsupported output format: {filetype}")
+            return data
+
+        def _read(path, filetype, gzip, header):
+
+            if filetype == "parquet":
+                data = pd.read_parquet(path=path, filesystem=self.filesystem)
+            else:
+                with self.filesystem.open(path, mode="rb") as file_object:
+                    data = _deserialize(
+                        data=file_object.read(),
+                        filetype=filetype,
+                        gzip=gzip,
+                        header=header,
+                    )
+            return data
+
+        with ThreadPoolExecutor() as pool:
+            data = list(
+                pool.map(
+                    lambda x: _read(
+                        path=x,
+                        filetype=filetype,
+                        gzip=gzip,
+                        header=header,
+                    ),
+                    self.filesystem.find(path=path),
+                )
+            )
+
+        return (
+            pd.concat(data).reset_index(drop=True) if len(data) > 0 else pd.DataFrame()
+        )
+
+    def write(
+        self,
+        dataframe,
+        path,
+        filetype="parquet",
+        gzip=False,
+        header=False,
+        partition_by=None,
+        suffix=None,
+        drop_partitions=False,
+    ):
+        """
+        Saves data in a given format
+        :param dataframe: input pandas.DataFrame
+        :param path: path-like
+        :param filetype: output format: parquet|tsv|jsonlines
+        :param gzip: should the output be gzipped: tsv|jsonlines
+        :param header: should the columns names be included: tsv-only
+        :param partition_by: optional, list of columns to partition the output
+        :param suffix: optional, suffix to use for output partitions
+        :param drop_partitions: optional, bool - if to drop partition fields from output
+        """
+
+        def _serialize(dataframe, filetype, gzip, header):
+            if filetype == "tsv":
+                data = dataframe.to_csv(sep="\t", index=False, header=header)
+            elif filetype == "jsonlines":
+                data = dataframe.to_json(orient="records", lines=True)
+            else:
+                raise ValueError(f"Unsupported output format: {filetype}")
+            data = data.encode("utf-8")
+            if gzip:
+                data = compress(data)
+            return data
+
+        def _write(path, dataframe, filetype, gzip, header):
+            self.filesystem.makedirs(os.path.dirname(path), exist_ok=True)
+            if filetype == "parquet":
+                dataframe.to_parquet(path=path, filesystem=self.filesystem)
+            else:
+                data = _serialize(
+                    dataframe=dataframe, filetype=filetype, gzip=gzip, header=header
+                )
+                with self.filesystem.open(path, mode="wb") as file_object:
+                    file_object.write(data)
+
+        pipeline = self.get_partitions(
+            dataframe=dataframe,
+            partition_by=partition_by,
+            prefix=path,
+            suffix=suffix,
+            drop=drop_partitions,
+        )
+
+        with ThreadPoolExecutor() as pool:
+            list(
+                pool.map(
+                    lambda x: _write(
+                        path=x[0],
+                        dataframe=x[1],
+                        filetype=filetype,
+                        gzip=gzip,
+                        header=header,
+                    ),
+                    pipeline,
+                )
+            )
--- tests/test_io.py
+++ tests/test_io.py
@@ -6,24 +6,20 @@ import datetime
 
 import boto3
 import pytest
+import pandas as pd
 from moto import mock_s3, mock_sts
 
 
 @mock_s3
 @mock_sts
-class TestIO(unittest.TestCase):
+class TestFileSystem(unittest.TestCase):
     def setUp(self):
         # Create a temporary directory
         self.test_dir = tempfile.mkdtemp()
 
-        self.bucket_name = self.test_dir.split("/")[1]
+        self.bucket_name = "test_bucket"
         s3 = boto3.client("s3")
         s3.create_bucket(Bucket=self.bucket_name)
-
-        os.environ["AWS_ACCESS_KEY_ID"] = "testing"
-        os.environ["AWS_SECRET_ACCESS_KEY"] = "testing"
-        os.environ["AWS_SECURITY_TOKEN"] = "testing"
-        os.environ["AWS_SESSION_TOKEN"] = "testing"
 
         self.basic_params = [
             ("local", self.test_dir),
@@ -117,3 +113,259 @@ class TestIO(unittest.TestCase):
                 fs._rm(path=path1)
                 ls = fs.ls(path=path)
                 assert len(ls) == 1
+
+
+@mock_s3
+class TestDataIO(unittest.TestCase):
+    def setUp(self):
+        # Create a temporary directory
+        self.test_dir = tempfile.mkdtemp()
+
+        self.bucket_name = "test_bucket"
+        s3 = boto3.client("s3")
+        s3.create_bucket(Bucket=self.bucket_name)
+
+        self.sample_df = pd.DataFrame(
+            {
+                "col1": ["a", "a", "b", "b", "b"],
+                "col2": [1, 1, 1, 1, 2],
+                "col3": [1, None, 123, -42, 0],
+            }
+        )
+
+        self.params = [
+            {
+                "filesystem": "local",
+                "path": self.test_dir,
+                "filetype": "tsv",
+                "gzip": False,
+                "header": True,
+            },
+            {
+                "filesystem": "local",
+                "path": self.test_dir,
+                "filetype": "tsv",
+                "gzip": False,
+                "header": False,
+            },
+            {
+                "filesystem": "local",
+                "path": self.test_dir,
+                "filetype": "tsv",
+                "gzip": True,
+                "header": True,
+            },
+            {
+                "filesystem": "local",
+                "path": self.test_dir,
+                "filetype": "tsv",
+                "gzip": True,
+                "header": False,
+            },
+            {
+                "filesystem": "local",
+                "path": self.test_dir,
+                "filetype": "jsonlines",
+                "gzip": True,
+            },
+            {
+                "filesystem": "local",
+                "path": self.test_dir,
+                "filetype": "jsonlines",
+                "gzip": False,
+            },
+            {"filesystem": "local", "path": self.test_dir, "filetype": "parquet"},
+            {
+                "filesystem": "s3",
+                "path": self.bucket_name,
+                "filetype": "tsv",
+                "gzip": False,
+                "header": True,
+            },
+            {
+                "filesystem": "s3",
+                "path": self.bucket_name,
+                "filetype": "tsv",
+                "gzip": False,
+                "header": False,
+            },
+            {
+                "filesystem": "s3",
+                "path": self.bucket_name,
+                "filetype": "tsv",
+                "gzip": True,
+                "header": True,
+            },
+            {
+                "filesystem": "s3",
+                "path": self.bucket_name,
+                "filetype": "tsv",
+                "gzip": True,
+                "header": False,
+            },
+            {
+                "filesystem": "s3",
+                "path": self.bucket_name,
+                "filetype": "jsonlines",
+                "gzip": True,
+            },
+            {
+                "filesystem": "s3",
+                "path": self.bucket_name,
+                "filetype": "jsonlines",
+                "gzip": False,
+            },
+            {"filesystem": "s3", "path": self.bucket_name, "filetype": "parquet"},
+        ]
+
+    def tearDown(self):
+        # Remove the directory after the test
+        shutil.rmtree(self.test_dir)
+
+    def test_write_read_basics(self):
+        from datatoolz.io import DataIO, FileSystem
+
+        for i, params in enumerate(self.params):
+
+            filesystem = FileSystem(params["filesystem"])
+
+            dio = DataIO(filesystem=filesystem)
+
+            path = os.path.join(
+                params["path"],
+                str(i),
+                params["filetype"],
+                f"my-file.{params['filetype']}",
+            )
+
+            dio.write(
+                dataframe=self.sample_df,
+                path=path,
+                filetype=params["filetype"],
+                gzip=params.get("gzip"),
+                header=params.get("header"),
+            )
+
+            df = dio.read(
+                path=path,
+                filetype=params["filetype"],
+                gzip=params.get("gzip"),
+                header=params.get("header"),
+            )
+
+            print(params)
+            assert self.sample_df.shape == df.shape
+
+    def test_unsupported_filetype(self):
+        from datatoolz.io import DataIO
+
+        path = os.path.join(self.test_dir, "myfile.parquet")
+
+        dio = DataIO()
+        with pytest.raises(ValueError):
+            dio.write(
+                dataframe=self.sample_df, path=self.test_dir, filetype="unsupported"
+            )
+
+        dio.write(dataframe=self.sample_df, path=path)
+        with pytest.raises(ValueError):
+            dio.read(path=path, filetype="unsupported")
+
+    def test_write_read_with_partitions(self):
+        from datatoolz.io import FileSystem, DataIO
+
+        filesystem = FileSystem()
+        dio = DataIO(filesystem=filesystem)
+
+        partition_by = [self.sample_df.columns[0]]
+
+        path = os.path.join(self.test_dir, "parts1", "my-file")
+        dio.write(dataframe=self.sample_df, path=path, partition_by=partition_by)
+        files = filesystem.find(path=path)
+        assert len(files) == self.sample_df[partition_by].drop_duplicates().shape[0]
+        df = dio.read(path=files[0])
+        head, suffix = os.path.split(files[0])
+        col, val = os.path.split(head)[1].split("=")
+        assert df.shape[0] == (self.sample_df[col] == val).sum()
+
+        path = os.path.join(self.test_dir, "parts-drop", "my-file")
+        dio.write(
+            dataframe=self.sample_df,
+            path=path,
+            partition_by=partition_by,
+            drop_partitions=True,
+        )
+        df = dio.read(path=path)
+        assert df.shape == (
+            self.sample_df.shape[0],
+            self.sample_df.shape[1] - len(partition_by),
+        )
+        assert list(df.columns) == [
+            col for col in self.sample_df.columns if col not in partition_by
+        ]
+
+    def test_write_custom_partition_formatting(self):
+        from datatoolz.io import FileSystem, DataIO
+
+        filesystem = FileSystem()
+
+        def custom_formatter(prefix, partitions, values, suffix, drop_prefixes):
+            partitions = partitions or list()
+            values = values or list()
+            drop_prefixes = drop_prefixes or list()
+            return os.path.join(
+                prefix,
+                *(
+                    f"{value}" if field in drop_prefixes else f"{field}*{value}"
+                    for field, value in zip(partitions, values)
+                ),
+                "default" if suffix is None else suffix,
+            )
+
+        partition_by = [self.sample_df.columns[0], self.sample_df.columns[1]]
+        dio = DataIO(
+            filesystem=filesystem,
+            partition_transformer=lambda prefix, partitions, values, suffix: custom_formatter(
+                prefix,
+                partitions,
+                values,
+                suffix,
+                drop_prefixes=[self.sample_df.columns[0]],
+            ),
+        )
+
+        path = os.path.join(self.test_dir, "parts-custom-format", "my-file")
+        dio.write(
+            dataframe=self.sample_df,
+            path=path,
+            partition_by=partition_by,
+        )
+        files = filesystem.find(path=path)
+
+        partition_data = self.sample_df[partition_by].drop_duplicates()
+        assert len(files) == partition_data.shape[0]
+
+        files = [os.path.dirname(f) for f in files]
+
+        for _, x in partition_data.iterrows():
+            partition_name = list()
+            for i, col in enumerate(partition_by):
+                if i == 0:
+                    partition_name.append(x[col])
+                else:
+                    partition_name.append(f"{col}*{x[col]}")
+            partition_name = os.path.join(path, *partition_name)
+            assert partition_name in files
+
+        with pytest.raises(ValueError):
+            dio.get_path(
+                prefix="prefix", partitions=["a"], values=[1, 2], suffix="suffix"
+            )
+
+        with pytest.raises(ValueError):
+            dio.get_path(
+                prefix="prefix",
+                partitions=["a", "b"],
+                values=[1, None],
+                suffix="suffix",
+            )
