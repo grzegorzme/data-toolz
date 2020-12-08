7a315060bc307a740e21c1a3f1bb89e9529057ee
Grzegorz Melniczak <gmelniczak@olx.pl>
2020-12-08 14:29:10 +0100

    change author details

--- setup.py
+++ setup.py
@@ -9,13 +9,13 @@ with open("requirements.txt") as f:
 
 setuptools.setup(
     name="data-toolz",
-    version="0.1.2",
+    version="0.1.3",
     author="Grzegorz Melniczak",
-    author_email="gmelniczak@olx.pl",
+    author_email="mogadish@gmail.com",
     description="Data helper package",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://git.naspersclassifieds.com/shared-services/data-science/data-toolz.git",
+    url="https://github.com/grzegorzme/data-toolz",
     packages=setuptools.find_packages(exclude=["tests"]),
     classifiers=["Programming Language :: Python :: 3"],
     python_requires=">=3.7",
