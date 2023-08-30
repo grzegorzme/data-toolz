a746f73a36fe26794d6fcefa689581167d42342b
Grzegorz Melniczak <gmelniczak@olx.pl>
2023-08-30 15:16:34 +0200

    bump package version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ requirements = ["s3fs==0.4.2", "pandas", "pyarrow"]
 
 setuptools.setup(
     name="data-toolz",
-    version="0.1.10",
+    version="0.1.11",
     author="Grzegorz Melniczak",
     author_email="mogadish@gmail.com",
     description="Data helper package",
